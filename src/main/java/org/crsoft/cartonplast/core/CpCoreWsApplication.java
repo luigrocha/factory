d35a5b47c84e08c5862d6f45965819d0035fe38c
Jonathan Yépez <jyepez@ec.krugercorp.com>
2022-04-26 19:55:30 -0500

    CARPLAST-62 Add Spring Boot

---
+++ src/main/java/org/crsoft/cartonplast/core/CpCoreWsApplication.java
@@ -0,0 +1,13 @@
+package org.crsoft.cartonplast.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpCoreWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpCoreWsApplication.class, args);
+	}
+
+}
