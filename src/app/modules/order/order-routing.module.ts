55b62a27ed1658fc93ec122d396808098e16003b
Jonathan Yépez <jyepez@ec.krugercorp.com>
2022-05-16 20:38:06 -0500

    Add module order-status

--- src/app/modules/order/order-routing.module.ts
+++ src/app/modules/order/order-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DiesListComponent } from 'src/app/modules/dies/pages/dies-list/dies-list.component';
 import { OrderComponent } from './pages/order-list/order.component';
+import { OrderStatusComponent } from './pages/order-status/order-status.component';
 
 const routes: Routes = [
   {
     path: '',
     component: OrderComponent,
+  },
+  {
+    path: 'status/:lote',
+    component: OrderStatusComponent,
   }
 ];
 
