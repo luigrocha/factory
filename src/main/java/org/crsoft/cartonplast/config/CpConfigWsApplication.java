89a51eb8f883284e3f376f127827d4c3a6586e41
Luis Pillaga <lapillaga@espe.edu.ec>
2022-05-19 21:59:47 -0500

    Initial commit

---
+++ src/main/java/org/crsoft/cartonplast/config/CpConfigWsApplication.java
@@ -0,0 +1,13 @@
+package org.crsoft.cartonplast.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpConfigWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpConfigWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/crsoft/cartonplast/config/CpConfigWsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crsoft.cartonplast.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpConfigWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
