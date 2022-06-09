75b24d79b01c320f5d059d23f52ef970159d1eb3
Jonathan Yépez <jyepez@ec.krugercorp.com>
2022-06-08 22:40:05 -0500

    CARPLAST-136 add crud locations

--- cp-celler-ws/src/main/java/org/crsoft/cartonplast/celler/controller/LocationController.java
+++ cp-celler-ws/src/main/java/org/crsoft/cartonplast/celler/controller/LocationController.java
@@ -1,13 +1,14 @@
 package org.crsoft.cartonplast.celler.controller;
 
+import org.crsoft.cartonplast.celler.model.Location;
 import org.crsoft.cartonplast.celler.service.ILocationService;
 import org.crsoft.cartonplast.common.constant.GlobalConstant;
+import org.crsoft.cartonplast.common.exception.InsertException;
 import org.crsoft.cartonplast.common.exception.NotFoundException;
+import org.crsoft.cartonplast.common.exception.UpdateException;
 import org.crsoft.cartonplast.vo.res.LocationRes;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -28,4 +29,34 @@ public class LocationController {
     public ResponseEntity<Collection<LocationRes>> findAllLocation() throws NotFoundException {
         return ResponseEntity.ok(this.locationService.findAllLocation());
     }
+
+    @PostMapping
+    public ResponseEntity<?> createLocation(@RequestBody Location location, @RequestHeader("userName") String userName) throws InsertException {
+        location.setCreatedBy(userName);
+        this.locationService.createLocation(location);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{code}")
+    public ResponseEntity<LocationRes> findLocationByCode(@PathVariable("code") Integer code) throws NotFoundException {
+        return ResponseEntity.ok(this.locationService.findLocationByCode(code));
+    }
+
+    @PatchMapping("/{code}")
+    public ResponseEntity<?> updateLocationByCode(
+            @PathVariable("code") Integer code,
+            @RequestBody Location location,
+            @RequestHeader("userName") String userName) throws NotFoundException, UpdateException {
+        location.setUpdatedBy(userName);
+        this.locationService.updateLocationByCode(code, location);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{code}")
+    public ResponseEntity<?> deleteLocationByCode(@PathVariable("code") Integer code, @RequestHeader("userName") String userName)
+            throws NotFoundException, UpdateException {
+        this.locationService.deleteLocationByCode(code, userName);
+        return ResponseEntity.ok().build();
+    }
+
 }
--- cp-celler-ws/src/main/java/org/crsoft/cartonplast/celler/service/impl/LocationService.java
+++ cp-celler-ws/src/main/java/org/crsoft/cartonplast/celler/service/impl/LocationService.java
@@ -5,15 +5,18 @@ import org.crsoft.cartonplast.celler.model.Location;
 import org.crsoft.cartonplast.celler.repository.LocationRepository;
 import org.crsoft.cartonplast.celler.service.ILocationService;
 import org.crsoft.cartonplast.celler.service.mapper.LocationMapper;
+import org.crsoft.cartonplast.common.exception.InsertException;
 import org.crsoft.cartonplast.common.exception.NotFoundException;
+import org.crsoft.cartonplast.common.exception.UpdateException;
 import org.crsoft.cartonplast.vo.res.LocationRes;
 import org.keycloak.common.util.CollectionUtil;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Optional;
 
-import static org.crsoft.cartonplast.common.constant.MessagesConstant.MESSAGE_NOT_FOUND;
+import static org.crsoft.cartonplast.common.constant.MessagesConstant.*;
 
 /**
  * @author jyepez on 31/5/2022
@@ -22,6 +25,7 @@ import static org.crsoft.cartonplast.common.constant.MessagesConstant.MESSAGE_NO
 @Slf4j
 public class LocationService implements ILocationService {
 
+    private static final String TABLE_NAME = "CDTLOC";
     private final LocationRepository locationRepository;
     private final LocationMapper locationMapper;
 
@@ -38,6 +42,50 @@ public class LocationService implements ILocationService {
         } else {
             log.error("Error to findAllLocation");
             throw new NotFoundException(MESSAGE_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public void createLocation(Location location) throws InsertException {
+        try {
+            this.locationRepository.save(location);
+        } catch (Exception e) {
+            log.error("Error to createLocation: {}", e.getMessage());
+            throw new InsertException(TABLE_NAME, MESSAGE_INSERT);
+        }
+    }
+
+    @Override
+    public LocationRes findLocationByCode(Integer code) throws NotFoundException {
+        return this.locationMapper.locationToLocationRes(getLocationByCode(code));
+    }
+
+    @Override
+    public void updateLocationByCode(Integer code, Location location) throws NotFoundException, UpdateException {
+        Location locationByCode = getLocationByCode(code);
+        try {
+            locationByCode.setLocation(location.getLocation());
+            locationByCode.setDescription(location.getDescription());
+            locationByCode.setUpdatedBy(location.getUpdatedBy());
+            locationByCode.setUpdatedAt(LocalDateTime.now());
+            this.locationRepository.save(locationByCode);
+        } catch (Exception e) {
+            log.error("Error to updateLocationByCode: {}", e.getMessage());
+            throw new UpdateException(TABLE_NAME, MESSAGE_UPDATE);
+        }
+    }
+
+    @Override
+    public void deleteLocationByCode(Integer code, String userName) throws NotFoundException, UpdateException {
+        Location location = getLocationByCode(code);
+        try {
+            location.setUpdatedBy(userName);
+            location.setUpdatedAt(LocalDateTime.now());
+            location.setValidTo(LocalDateTime.now());
+            this.locationRepository.save(location);
+        }catch (Exception e) {
+            log.error("Error to deleteLocationByCode: {}", e.getMessage());
+            throw new UpdateException(TABLE_NAME, MESSAGE_DELETE);
         }
     }
 
