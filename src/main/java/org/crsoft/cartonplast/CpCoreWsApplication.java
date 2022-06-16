687a581c6e2e683d37845f00c32a6cc7e1f44727
Luis Pillaga <lapillaga@espe.edu.ec>
2022-06-15 21:32:48 -0500

    fix: set timezone to america guayaquiul

--- src/main/java/org/crsoft/cartonplast/CpCoreWsApplication.java
+++ src/main/java/org/crsoft/cartonplast/CpCoreWsApplication.java
@@ -7,12 +7,21 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EntityScan(basePackages = {"org.crsoft.cartonplast"})
 @SpringBootApplication(scanBasePackages = {"org.crsoft.cartonplast.*"})
 @EnableJpaRepositories(basePackages = {"org.crsoft.cartonplast"})
 @EnableFeignClients(basePackages = {"org.crsoft.cartonplast"})
 @EnableJpaAuditing
 public class CpCoreWsApplication {
+
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot SetTimeZone
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Guayaquil"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CpCoreWsApplication.class, args);
