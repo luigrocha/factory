e9d312b723b755eb4c9a066af0a8d2ed02edcb31
Luis Pillaga <lapillaga@espe.edu.ec>
2022-06-24 06:12:05 -0500

    Add project list api

---
+++ cp-design-ws/src/main/java/org/crsoft/cartonplast/design/service/mapper/ProjectMapper.java
@@ -0,0 +1,28 @@
+package org.crsoft.cartonplast.design.service.mapper;
+
+import org.crsoft.cartonplast.client.service.mapper.ClientMapper;
+import org.crsoft.cartonplast.design.model.Project;
+import org.crsoft.cartonplast.design.vo.res.ProjectRes;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+/**
+ * @author lpillaga on 23/06/2022
+ */
+@Mapper(componentModel = "spring", uses = {
+        ClientMapper.class,
+        HomoPolymerMapper.class,
+        ColorBMapper.class,
+        CyrelMapper.class,
+        TalcMapper.class,
+        CalciumCarbonateMapper.class,
+        DieProductMapper.class,
+        ProjectTypeMapper.class
+})
+public interface ProjectMapper {
+
+    ProjectRes toProjectRes(Project project);
+
+    List<ProjectRes> toProjectResList(List<Project> projectList);
+}
---
+++ cp-design-ws/src/main/java/org/crsoft/cartonplast/design/vo/res/TalcRes.java
@@ -0,0 +1,22 @@
+package org.crsoft.cartonplast.design.vo.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author lpillaga on 23/06/2022
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TalcRes implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private Integer percentage;
+    private String lpCode;
+}
