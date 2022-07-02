4dd6c7fc2c9d523521a105297f6f183fe959346e
Jonathan Yépez <jyepez@ec.krugercorp.com>
2022-07-01 20:47:25 -0500

    CARPLAST-236 MERGE TO CARPLAST-233

---
+++ src/main/java/org/crsoft/cartonplast/catalog/service/ICatalogPriorityService.java
@@ -0,0 +1,14 @@
+package org.crsoft.cartonplast.catalog.service;
+
+import org.crsoft.cartonplast.vo.res.CatalogPriorityRes;
+
+import java.util.List;
+
+/**
+ * @author jyepez on 25/5/2022
+ */
+public interface ICatalogPriorityService {
+
+    List<CatalogPriorityRes> findAllPriorityByType(String type);
+
+}
