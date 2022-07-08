9409e388213f2040c40de970cc3d49d7a22762bc
Luis Pillaga <lapillaga@espe.edu.ec>
2022-07-07 19:44:02 -0500

    Add development configuration

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+
+  appApiUrl: 'https://webapp-dev-api.carton-plast.com/api/v1',
+  userApi: 'https://users-dev.carton-plast.com/api/v1',
+  configApiUrl: 'https://config-api-dev.carton-plast.com/api/v1',
+  imagesBucketName: 'images-dev',
+  keycloak: {
+    url: 'https://auth-test.carton-plast.com',
+    realm: 'carton-plast',
+    clientId: 'carton-plast-frontend'
+  },
+};
